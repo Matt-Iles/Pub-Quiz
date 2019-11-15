c77a83751d57c60660bdff09ffbb5442541c68b2
ILES Matthew <miles@one.steria.dom>
2019-11-15 15:48:20 +0000

    starting project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="https://pbs.twimg.com/profile_images/1161653602806575105/BiccqAlf_400x400.jpg" width="100px" height="100"   >
+    <HelloWorld msg="" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/home.vue";
 
 export default {
   name: "app",
@@ -16,13 +16,19 @@ export default {
 };
 </script>
 
+
+
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #111; 
+}
+
+#para1{
+  color:#111;
+  margin-left: -1000px;
 }
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Quiz</h1>
+    <div>
+      <router-link to="">Go to secure page</router-link>
+    </div>
+
+    <div>
+      <p1 id="para1">
+        Hi
+      </p1>
+    </div>
+      
+
+  </div>
+</template>
+
+
+
